559dbd7c2e89819ac7517945e644dd1861b98a69
Alyshia Ledlie <alyshialedlie@gmail.com>
2026-03-11 16:15:33 -0500

    added vim plugins

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -172,13 +172,101 @@ export GITHUB_TOKEN="$(doppler_get "GITHUB_TOKEN")"
 
 # -------------------- Convenience Aliases ---------------- #
 
-# Basic aliases
-alias ll='ls -al'
-alias la='ls -A'
-alias l='ls -CF'
+# Color output for ls
+colorflag="-G"
+alias ls='gls --color=auto'
+export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+
+# Listing variants
+alias l="ls -lF ${colorflag}"
+alias ll="gls -lv --group-directories-first"
+alias lm="ll --color |more"
+alias lr="ll -r --color"
+alias la="ll -laf --color"
+alias tree="tree -Csuh"
 
 # Enable aliases to be sudo'ed
 alias sudo='sudo '
+
+# Python shortcut
+alias python=python3
+
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias home="cd ~"
+alias -- -="cd -"
+
+# Shortcuts
+alias dropbox="cd ~/Dropbox\ \(Personal\)"
+alias downloads="cd ~/Downloads"
+alias desktop="cd ~/Desktop"
+alias code="cd ~/code"
+alias h="history"
+
+# Common project paths
+alias jobs="~/code/jobs"
+alias integrity-ai="~/code/is-public-sites/IntegrityStudioLandingPage"
+alias obtool="~/.claude/mcp-servers/observability-toolkit"
+alias obtool-ui="~/.claude/mcp-servers/observability-toolkit/dashboard"
+alias tcad="~/code/is-public-sites/tcad-scraper"
+alias blog="~/code/PersonalSite"
+alias reports="~/reports"
+alias ast="~/code/ast-grep-mcp"
+
+# Doppler shorthand
+alias doppler="doppler secrets --project integrity-studio --config dev --"
+
+# IP addresses
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Clipboard
+alias c="tr -d '\n' | pbcopy"
+
+# Cleanup
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
+
+# Intuitive map function
+alias map="xargs -n1"
+
+# Get week number
+alias week='date +%V'
+
+# Stopwatch
+alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+
+# Reload shell
+alias reload="exec $SHELL -l"
+
+# Volume control
+alias stfu="osascript -e 'set volume output muted true'"
+alias pumpitup="osascript -e 'set volume 7'"
+
+# Kill Chrome renderer tabs
+alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+
+# Lock screen
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+# OS X fallbacks
+command -v md5sum > /dev/null || alias md5sum="md5"
+command -v sha1sum > /dev/null || alias sha1sum="shasum"
+
+# URL-encode strings
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+
+# Merge PDF files
+alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
+
+# PlistBuddy
+alias plistbuddy="/usr/libexec/PlistBuddy"
+
+# HTTP method aliases
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+	alias "$method"="lwp-request -m '$method'"
+done
 
 # macOS specific aliases
 if [[ "$PLATFORM" == "macos" ]]; then
@@ -195,12 +283,18 @@ if [[ "$PLATFORM" == "macos" ]]; then
     # Network monitoring
     alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
     alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+    # Trash and system logs
+    alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+
+    # Finder visibility
+    alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+    alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+    alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+    # LaunchServices cleanup
+    alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+
+    # Spotlight
+    alias spoton="sudo mdutil -a -i on"
 fi
-
-# IP addresses
-alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
-
-# Development
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
